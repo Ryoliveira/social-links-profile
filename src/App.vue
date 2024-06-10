85b19636207f26f63008fc51ae9eb33174c10778
Ryan Oliveira <ryoliveira91@gmail.com>
2024-06-09 20:08:39 -0700

    challenge complete

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import SocialLinkCard from './components/SocialLinkCard.vue';
+import avatarImg from './assets/avatar-ryan.jpg';
+
+const cardInfo = {
+  name: "Ryan Oliveira",
+  avatar: avatarImg,
+  residence: "California, United States",
+  bio: "\"Front-end developer and avid gamer.\"",
+  links: [
+    {
+      title: 'GitHub',
+      url: 'https://github.com/Ryoliveira'
+    },
+    {
+      title: 'Frontend Mentor',
+      url: 'https://www.frontendmentor.io/profile/Ryoliveira'
+    },
+    {
+      title: 'LinkedIn',
+      url: '#'
+    },
+    {
+      title: 'Twitter',
+      url: '#'
+    },
+    {
+      title: 'Instagram',
+      url: '#'
+    }
+  ],
+}
+</script>
+
+<template>
+  <SocialLinkCard :card-info="cardInfo" />
+</template>
---
+++ src/components/SocialLinkCard.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <!-- container -->
+    <div class="container">
+
+        <!-- avatar -->
+        <img class="avatar" :src="cardInfo.avatar" alt="Avatar Photo"/>
+
+        <!-- name -->
+        <p class="name">{{ cardInfo.name }}</p>
+        <!-- location -->
+        <p class="residence">{{ cardInfo.residence }}</p>
+
+        <!-- bio -->
+        <p class="bio">{{ cardInfo.bio }}</p>
+
+        <!-- links -->
+        <div @click="redirect(link.url)" class="link" v-for="link in cardInfo.links">
+            <span class="link-text">{{ link.title }}</span>
+        </div>
+
+    </div>
+
+</template>
+
+<script setup lang="ts">
+const props = defineProps({ cardInfo: Object });
+
+console.log(props.cardInfo);
+
+const redirect = (url) => {
+    window.location.href = url;
+}
+
+console.log
+</script>
+
+<style src="../scss/SocialLinkCard.scss" scoped />
